8d3044a28e6666131b43804ee278e7290e5c1b80
taiyebnirjhar <nirjhar.212902003@green.ac.bd>
2023-11-25 22:38:54 +0600

    initialised bcrypt

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,4 +1,6 @@
+import bcrypt from "bcrypt";
 import { Schema } from "mongoose";
+import config from "../../config";
 import IUser, {
   ExtendedUserModel,
   IAddress,
@@ -9,27 +11,80 @@ import { UserModel } from "./user.model";
 const fullNameSchema = new Schema<IUserFullName>({
   firstName: {
     type: String,
-    required: true,
+    required: [true, "First name is required."],
   },
-  lastName: { type: String, required: true },
+  lastName: {
+    type: String,
+    required: [true, "Last name is required."],
+  },
 });
 
 const addressSchema = new Schema<IAddress>({
-  street: { type: String, required: true },
-  city: { type: String, required: true },
-  country: { type: String, required: true },
+  street: {
+    type: String,
+    required: [true, "Street is required."],
+  },
+  city: {
+    type: String,
+    required: [true, "City is required."],
+  },
+  country: {
+    type: String,
+    required: [true, "Country is required."],
+  },
 });
 
 const userSchema = new Schema<IUser, ExtendedUserModel>({
-  userId: { type: Number, required: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  userId: {
+    type: Number,
+    required: [true, "User ID is required."],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required."],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+  },
   fullName: fullNameSchema,
-  age: { type: Number, required: true },
-  email: { type: String, required: true },
-  isActive: { type: Boolean, required: true },
-  hobbies: [{ type: String }],
+  age: {
+    type: Number,
+    required: [true, "Age is required."],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    validate: {
+      validator: (value: string) => /\S+@\S+\.\S+/.test(value),
+      message: "Invalid email format.",
+    },
+  },
+  isActive: {
+    type: Boolean,
+    required: [true, "isActive is required."],
+  },
+  hobbies: {
+    type: [String],
+    required: [true, "Hobbies are required."],
+  },
   address: addressSchema,
+});
+
+userSchema.pre("save", async function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
+});
+
+userSchema.post("save", function (doc, next) {
+  doc.password = "";
+  next();
 });
 
 userSchema.statics.isUserExists = async function (userId: number) {
